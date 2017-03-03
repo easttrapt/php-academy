0e521218270145e313cc535a0a69219fb7a87c73
Yuriy K <easttrapt@gmail.com>
2017-03-03 18:26:28 +0200

    task 8 done. Bug with old get-request in url fixed by header redirect

---
+++ functions_forms_tasks/8/index.php
@@ -0,0 +1,187 @@
+<?php
+//Создать гостевую книгу, где любой человек может оставить комментарий в текстовом поле и добавить его. Все добавленные комментарии выводятся над текстовым полем. 
+//Реализовать проверку на наличие в тексте запрещенных слов, матов. При наличии таких слов - выводить сообщение "Некорректный комментарий". Реализовать удаление из комментария всех тегов, кроме тега &lt;b&gt;.
+
+error_reporting(E_ALL);
+
+//functions
+function isRequestPost(){
+	return (bool) $_POST;
+}
+
+
+function isFormNotEmpty(){
+	return trim($_POST['userName']) && trim($_POST['userComment']);
+}
+
+function requestPost($key, $template = ''){
+	return isset($_POST[$key]) ? $_POST[$key] : $template;
+}
+
+function addComment($arr){
+	$arr[] = array(trim($_POST['userName']) => trim($_POST['userComment']));
+}
+
+//for delete
+function isRequestGet(){
+	return (bool) $_GET;
+}
+
+function isActionDelete(){
+	return $_GET['action'] == 'delete' && $_GET['id'];
+}
+
+function deleteComment($file, $id){
+	$comments = getCommentsArr($file);
+	foreach ($comments as $key => $comment) {
+		if ($comments[$key]["id"] == $id) {
+			unset($comments[$key]);
+		}
+	}
+	//сохраняю в json
+	foreach ($comments as $key => $comment) {
+		$comments[$key] = json_encode($comment);
+	}
+	$comments = implode(PHP_EOL, $comments);
+	file_put_contents($file, $comments);
+}
+
+
+function debug($a){
+	echo "<pre>";
+	var_dump($a);
+	echo "</pre>";
+}
+
+function saveComment($file, array $comment){
+	$comments = file_get_contents($file) . json_encode($comment) . PHP_EOL;
+	file_put_contents($file, $comments);
+}
+
+function getCommentsArr($file){
+	$comments = file_get_contents($file);
+	$result = [];
+	if ($comments) {
+		$comments = array_filter(explode(PHP_EOL, $comments));
+		foreach ($comments as $key => $commentString) {
+			$result[$key] = json_decode($commentString, true);
+		}
+	}
+	return $result;
+}
+
+//check for obscene
+function ifObscene($str){
+	//так как цель не указана, буду считать, что нужно не потерять лицензию СМИ РФ
+	//http://www.interfax.ru/russia/348755
+	//список сократил до примитивного
+	$bads = [
+		"хуй",
+		"пизда",
+		"шлюха",
+		"ебать"
+	];
+
+	$str = trim($str);
+	$pattern = "/[\s-_!,.:+=?]+/";
+	$stringArr = preg_split($pattern, $str);
+
+	return !empty(array_uintersect($bads, $stringArr, "strcasecmp"));
+}
+
+//clear html tags
+//string strip_tags ( string $str [, string $allowable_tags ] )
+
+
+//logic
+$fileComments = './comments.txt';
+$errorMessage = '';
+
+//get saved comments from file
+$allComments = getCommentsArr($fileComments);
+
+//add new comment
+if(isRequestPost()) {
+	if(isFormNotEmpty()) {
+		if (!ifObscene(trim($_POST['userComment']))) {
+			$newComment = array(
+				'id' => uniqid(),
+				'userName' => trim($_POST['userName']),
+				'userComment' => strip_tags(trim($_POST['userComment']), "<b>")
+				);
+
+			saveComment($fileComments, $newComment);
+			//get comments again
+			$allComments = getCommentsArr($fileComments);
+		} else {
+			$errorMessage = "Offensive detected";
+		}
+	} else {
+		$errorMessage = "Form is invalid";
+	}
+}
+
+//delete comment
+if(isRequestGet()){
+	if(isActionDelete()){
+		deleteComment($fileComments, $_GET['id']);
+		header('Location: ./index.php');
+
+	} else {
+		$errorMessage = "Cant delete";
+	}
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>getCommonWords</title>
+	<style>
+		.comment:empty {
+			display: none;
+		}
+		.comment {
+			max-width: 50%;
+			background-color: #dddddd;
+			margin: 5px;
+			padding: 5px;
+		}
+	</style>
+</head>
+<body>
+	<h1>Comments here</h1>
+	
+	<div class="comments">
+		<?php foreach($allComments as $index=>$userData): ?>
+		<div class="comment">
+			<h4><?=$userData["userName"]; ?></h4>
+			<span><?=$userData["userComment"]; ?></span><br>
+			<a href=<?="./?action=delete&id=".$userData["id"]; ?>>delete</a>
+		</div>
+		<?php endforeach; ?>
+
+	</div>
+
+	<form method="post">
+		<div>
+			<label for="userName">Your name</label>
+			<br>
+			<input type="text" name="userName" id="userName" value=<?="'".requestPost("userName")."'"; //"'" - не нашел более элегантного решения?>>
+		</div>
+
+		<div>
+			<label for="userComment">Your comment</label>
+			<br>
+			<textarea name="userComment" id="userComment" cols="30" rows="10"><?=requestPost("userComment"); ?></textarea>
+		</div>
+		<button>Send comment</button>
+	</form>
+	<br>
+
+	<h3><?=$errorMessage?></h3>
+
+</body>
+</html>
